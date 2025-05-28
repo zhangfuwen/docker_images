b26631c5e03159a36acc7d17ee9005ef9a230971
zhangfuwen <zhangfuwen@foxmail.com>
2025-05-28 17:38:50 +0800

    clean files in vim branch

--- install_gradle.sh
+++
@@ -1,8 +0,0 @@
-GRADLE_VERSION=$1
-
-export GRADLE_DIST=bin
-ZIP_FILE=gradle-${GRADLE_VERSION}-${GRADLE_DIST}.zip
-cd /opt && \
-    wget https://services.gradle.org/distributions/${ZIP_FILE} && \
-    unzip $ZIP_FILE && \
-    rm $ZIP_FILE 
